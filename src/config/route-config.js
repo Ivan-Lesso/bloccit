9e636934388b769012da4a14d9f706ab69f6c2c8
Ivan Zalessov <ivan.zalessov@gmail.com>
2018-11-17 14:41:37 -0600

    Updated routes assignment

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,8 +1,6 @@
 module.exports = {
   init(app) {
     const staticRoutes = require("../routes/static");
-    const marcoRoutes = require("../routes/marco");
-    app.use(marcoRoutes);
     app.use(staticRoutes);
   }
 }
