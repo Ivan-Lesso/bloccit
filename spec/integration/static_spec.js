fd9871f2c71073dff15863ac67f3460ed80d3d8e
Ivan Zalessov <ivan.zalessov@gmail.com>
2018-11-17 17:56:14 -0600

    Controllers and Views Checkpoint

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,10 +5,13 @@ const base = "http://localhost:3000/";
 describe("routes : static", () => {
   describe("GET /", () => {
     it("should return status code 200", (done) => {
-      request.get(base, (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        done();
-      });
+        request.get(base, (err, res, body) => {
+        it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+         request.get(base, (err, res, body) => {
+           expect(res.statusCode).toBe(200);
+           expect(body).toContain("Welcome to Bloccit");
+           done();
+         });
     });
   });
 });
