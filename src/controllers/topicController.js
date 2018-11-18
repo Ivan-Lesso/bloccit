9f09658a9179012f03132be9e5fbd003b4fe8999
Ivan Zalessov <ivan.zalessov@gmail.com>
2018-11-17 20:12:11 -0600

    Rule model

---
+++ src/controllers/topicController.js
@@ -0,0 +1,17 @@
+//#1
+ const topicQueries = require("../db/queries.topics.js");
+
+ module.exports = {
+   index(req, res, next) {
+//#2
+      topicQueries.getAllTopics((err, topics) => {
+
+//#3
+        if(err) {
+          res.redirect(500, "static/index");
+        } else {
+          res.render("topics/index", {title: "Topics", topics: topics});
+        }
+      })
+   }
+ }
